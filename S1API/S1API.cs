2c5e7f04a1eeb9d80433879885eb7828d5cf6191
KaBooMa <kaboomaz@gmail.com>
2025-04-21 00:39:52 -0500

    Removed example phone app from mod init

--- S1API/S1API.cs
+++ S1API/S1API.cs
@@ -1,5 +1,4 @@
 ﻿using MelonLoader;
-using S1API.PhoneApp;
 
 [assembly: MelonInfo(typeof(S1API.S1API), "S1API", "{VERSION_NUMBER}", "KaBooMa")]
 
@@ -7,21 +6,5 @@ namespace S1API
 {
     public class S1API : MelonMod
     {
-        public override void OnApplicationStart()
-        {
-            PhoneAppManager.Register(new MyAwesomeApp());
-        }
-        private bool _isInGame;
-
-        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
-        {
-            bool nowInGame = sceneName != null && sceneName.Contains("Main");
-
-            if (!_isInGame && nowInGame)
-            {
-                MelonLogger.Msg("Game scene loaded. Initializing phone apps...");
-                PhoneAppManager.InitAll(LoggerInstance);
-            }
-        }
     }
 }
