100422fbf6f68814165a06fa7a81fc8a9e97465f
KaBooMa <kaboomaz@gmail.com>
2025-04-21 00:46:36 -0500

    Initial release & deployment for ts (#8)
    * PhoneApp
    
    * * Implementation of new interfaces to help us internally
    * Misc internal additions for il2cpp / mono compatibility
    * Fixes to save and load processes
    
    * initial workflow deployment (#2)
    
    * initial workflow deployment
    
    * Added additional reruns for workflow
    
    * Updated assembly references
    
    * Updated assembly references
    
    * Updated proj path in workflow
    
    * Resolved PhoneApp deps
    
    * Disabled BuildUI in PhoneApp til @omar-akermi can review
    
    * Removed local post-build scripts
    
    * Fixed casing on deps
    
    * feat(semantic-verisioning): Added initial deployment functionality using semantic versioning
    
    Initial prep work for release deployment
    
    * Setup plugin to dynamically load S1API based on Il2Cpp/Mono (#6)
    
    * Added new S1APILoader.cs MelonPlugin. Mod now is intended to go inside Plugins/S1API. The plugin will load the correct version.
    * Added automated deployment while editing csproj files
    
    * Updated to build entire solution
    
    * Publishing (#7)
    
    publishing automation for stable
    
    * * Implementation of new interfaces to help us internally
    * Misc internal additions for il2cpp / mono compatibility
    * Fixes to save and load processes
    
    * PhoneApp
    
    * initial workflow deployment (#2)
    
    * initial workflow deployment
    
    * Added additional reruns for workflow
    
    * Updated assembly references
    
    * Updated assembly references
    
    * Updated proj path in workflow
    
    * Resolved PhoneApp deps
    
    * Disabled BuildUI in PhoneApp til @omar-akermi can review
    
    * Removed local post-build scripts
    
    * Fixed casing on deps
    
    * feat(semantic-verisioning): Added initial deployment functionality using semantic versioning
    
    Initial prep work for release deployment
    
    * Setup plugin to dynamically load S1API based on Il2Cpp/Mono (#6)
    
    * Added new S1APILoader.cs MelonPlugin. Mod now is intended to go inside Plugins/S1API. The plugin will load the correct version.
    * Added automated deployment while editing csproj files
    
    * Updated to build entire solution
    
    * Publishing (#7)
    
    publishing automation for stable
    
    * Added icon for ts deploy
    
    * Removed example phone app from mod init
    
    ---------
    
    Co-authored-by: Omar Akermi <mrakermi@gmail.com>

--- S1API/S1API.cs
+++ S1API/S1API.cs
@@ -1,26 +1,10 @@
 ﻿using MelonLoader;
-using S1API.PhoneApp;
-[assembly: MelonInfo(typeof(S1API.S1API), "S1API", "1.0.0", "KaBooMa")]
+
+[assembly: MelonInfo(typeof(S1API.S1API), "S1API", "{VERSION_NUMBER}", "KaBooMa")]
 
 namespace S1API
 {
     public class S1API : MelonMod
     {
-        public override void OnApplicationStart()
-        {
-            PhoneAppManager.Register(new MyAwesomeApp());
-        }
-        private bool _isInGame;
-
-        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
-        {
-            bool nowInGame = sceneName != null && sceneName.Contains("Main");
-
-            if (!_isInGame && nowInGame)
-            {
-                MelonLogger.Msg("Game scene loaded. Initializing phone apps...");
-                PhoneAppManager.InitAll(LoggerInstance);
-            }
-        }
     }
 }
