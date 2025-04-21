133f35492d31e4404a9dc3493640a1d0185d3c4a
KaBooMa <kaboomaz@gmail.com>
2025-04-21 00:51:22 -0500

    Revert "Initial release & deployment for ts (#8)" (#9)
    This reverts commit 100422fbf6f68814165a06fa7a81fc8a9e97465f.

--- S1API/S1API.cs
+++ S1API/S1API.cs
@@ -1,10 +1,26 @@
 ﻿using MelonLoader;
-
-[assembly: MelonInfo(typeof(S1API.S1API), "S1API", "{VERSION_NUMBER}", "KaBooMa")]
+using S1API.PhoneApp;
+[assembly: MelonInfo(typeof(S1API.S1API), "S1API", "1.0.0", "KaBooMa")]
 
 namespace S1API
 {
     public class S1API : MelonMod
     {
+        public override void OnApplicationStart()
+        {
+            PhoneAppManager.Register(new MyAwesomeApp());
+        }
+        private bool _isInGame;
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            bool nowInGame = sceneName != null && sceneName.Contains("Main");
+
+            if (!_isInGame && nowInGame)
+            {
+                MelonLogger.Msg("Game scene loaded. Initializing phone apps...");
+                PhoneAppManager.InitAll(LoggerInstance);
+            }
+        }
     }
 }
