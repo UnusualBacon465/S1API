63ab70cfbe165df3c6a219a02b29dd50256b8887
Omar Akermi <mrakermi@gmail.com>
2025-04-23 01:49:51 +0200

    fix: refactoring wrapper

--- S1API/Products/ProductDefinitionWrapper.cs
+++ S1API/Products/ProductDefinitionWrapper.cs
@@ -1,6 +1,6 @@
 using S1API.Internal.Utils;
 #if IL2CPP
-using S1Product = Il2CppScheduleOne.Product; // or ScheduleOne.Product, depending on the platform
+using S1Product = Il2CppScheduleOne.Product;
 #else
 using S1Product = ScheduleOne.Product;
 #endif
@@ -12,23 +12,12 @@ namespace S1API.Products
         public static ProductDefinition Wrap(ProductDefinition def)
         {
             var item = def.S1ItemDefinition;
-
-#if IL2CPP
             if (CrossType.Is<S1Product.WeedDefinition>(item, out var weed))
                 return new WeedDefinition(weed);
             if (CrossType.Is<S1Product.MethDefinition>(item, out var meth))
                 return new MethDefinition(meth);
             if (CrossType.Is<S1Product.CocaineDefinition>(item, out var coke))
                 return new CocaineDefinition(coke);
-#else
-            if (CrossType.Is<S1Product.WeedDefinition>(item, out var weed))
-                return new WeedDefinition(weed);
-            if (CrossType.Is<S1Product.MethDefinition>(item, out var meth))
-                return new MethDefinition(meth);
-            if (CrossType.Is<S1Product.CocaineDefinition>(item, out var coke))
-                return new CocaineDefinition(coke);
-#endif
-
             return def;
         }
     }
