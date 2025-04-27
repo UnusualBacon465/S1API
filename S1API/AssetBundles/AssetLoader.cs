b60479f030b8ac8f5b89ff6984af4e0393274d4d
Chloe Christley <734133+chloelcdev@users.noreply.github.com>
2025-04-27 12:25:20 -0400

    Turned AssetBundleHelper into AssetLoader, added WrappedAssetBundle
    also added WrappedAssetBundleRequest

---
+++ S1API/AssetBundles/AssetLoader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace S1API.AssetBundles
+{
+
+    public static class AssetLoader
+    {
+#if IL2CPP
+        /// <summary>
+        /// Loads an Il2Cpp AssetBundle from an embedded resource stream by name.
+        /// </summary>
+        /// <param name="fullResourceName">The full embedded resource name (including namespace path).</param>
+        /// <returns>The loaded Il2CppAssetBundle, or throws on failure.</returns>
+        public static  WrappedAssetBundle GetAssetBundleFromStream(string fullResourceName)
+        {
+            try
+            {
+                // Attempt to find the embedded resource in the executing assembly
+                var assembly = Assembly.GetExecutingAssembly();
+                using (Stream stream = assembly.GetManifestResourceStream(fullResourceName))
+                {
+                    if (stream == null)
+                        throw new Exception($"Embedded resource '{fullResourceName}' not found in {assembly.FullName}."); // hoping these throws will be melon/bepinex-agnostic
+
+                    // Read the stream into a byte array
+                    byte[] data = new byte[stream.Length];
+                    stream.Read(data, 0, data.Length);
+
+                    // Load the AssetBundle from memory
+                    Il2CppAssetBundle bundle = Il2CppAssetBundleManager.LoadFromMemory(data);
+                    if (bundle == null)
+                        throw new Exception($"Failed to load AssetBundle from memory: {fullResourceName}");
+
+                    return new(bundle);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions as needed
+                Debug.LogError($"Error loading AssetBundle from stream: {ex.Message}");
+                throw;
+            }
+        }
+#else
+        public static WrappedAssetBundle GetAssetBundleFromStream(string fullResourceName)
+        {
+            // Attempt to find the embedded resource in the executing assembly
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var stream = assembly.GetManifestResourceStream(fullResourceName);
+
+            return new(AssetBundle.LoadFromStream(stream));
+        }
+#endif
+
+        /// <summary>
+        /// 
+        /// No need to type the assembly just the path stuff. 
+        /// 
+        /// Example if carassetbundle is in subfolder /bundles/: 
+        /// 
+        ///         GameObject myCar = Instantiate(EasyLoad<GameObject>("bundles.carassetbundle", "MyCarGameObject"));
+        ///         
+        /// </summary>
+        public static T EasyLoad<T>(string bundle_name, string object_name) where T : UnityEngine.Object
+        {
+            return EasyLoad<T>(bundle_name, object_name, Assembly.GetExecutingAssembly(), out _);
+        }
+
+        public static T EasyLoad<T>(string bundle_name, string object_name, out WrappedAssetBundle bundle) where T : UnityEngine.Object
+        {
+            return EasyLoad<T>(bundle_name, object_name, Assembly.GetExecutingAssembly(), out bundle);
+        }
+
+        public static T EasyLoad<T>(string bundle_name, string object_name, Assembly assemblyOverride) where T : UnityEngine.Object
+        {
+            return EasyLoad<T>(bundle_name, object_name, assemblyOverride, out _);
+        }
+
+        public static T EasyLoad<T>(string bundle_name, string object_name, Assembly assemblyOverride, out WrappedAssetBundle bundle) where T : UnityEngine.Object
+        {
+            // Get the asset bundle from the assembly
+            bundle = GetAssetBundleFromStream($"{assemblyOverride.GetName().Name}.{bundle_name}");
+
+            // Load the asset from the bundle
+            return bundle.LoadAsset<T>(object_name);
+        }
+
+    }
+
+}
---
+++ S1API/AssetBundles/WrappedAssetBundle.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+
+#if IL2CPP
+using Type = Il2CppSystem.Type;
+using AssetBundle = UnityEngine.Il2CppAssetBundle;
+using AssetBundleRequest = UnityEngine.Il2CppAssetBundleRequest;
+#endif
+
+
+namespace S1API.AssetBundles
+{
+    
+    /// <summary>
+    ///     Works just like the AssetBundle type, it's just a proxy, but will use Il2CppAssetBundle if it needs to so you don't have to worry about it.
+    /// </summary>
+    public class WrappedAssetBundle
+    {
+        public bool isStreamedAssetBundle => _realBundle.isStreamedSceneAssetBundle;
+
+        public Object mainAsset => _realBundle.mainAsset;
+
+
+        public AssetBundle _realBundle;
+
+        public WrappedAssetBundle(AssetBundle realBundle)
+        {
+            _realBundle = realBundle;
+        }
+
+
+        public bool Contains(string name)
+        {
+            return _realBundle.Contains(name);
+        }
+
+        public string[] AllAssetNames()
+        {
+            return GetAllAssetNames();
+        }
+
+        public string[] GetAllAssetNames()
+        {
+            return _realBundle.GetAllAssetNames();
+        }
+
+        public string[] AllScenePaths()
+        {
+            return GetAllScenePaths();
+        }
+
+        public string[] GetAllScenePaths()
+        {
+            return _realBundle.GetAllScenePaths();
+        }
+
+        public Object Load(string name)
+        {
+            return LoadAsset(name);
+        }
+
+        public Object LoadAsset(string name)
+        {
+            return this.LoadAsset<Object>(name);
+        }
+
+        public T Load<T>(string name) where T : Object
+        {
+            return LoadAsset<T>(name);
+        }
+
+        public T LoadAsset<T>(string name) where T : Object
+        {
+            return _realBundle.LoadAsset<T>(name);
+        }
+
+        public Object Load(string name, Type type)
+        {
+            return LoadAsset(name, type);
+        }
+
+        public Object LoadAsset(string name, Type type)
+        {
+            return _realBundle.LoadAsset(name, type);
+        }
+
+        public WrappedAssetBundleRequest LoadAssetAsync(string name)
+        {
+            return this.LoadAssetAsync<Object>(name);
+        }
+
+        public WrappedAssetBundleRequest LoadAssetAsync<T>(string name) where T : Object
+        {
+            return new(_realBundle.LoadAssetAsync<T>(name));
+        }
+
+        public WrappedAssetBundleRequest LoadAssetAsync(string name, Type type)
+        {
+            return new(_realBundle.LoadAssetAsync(name, type));
+        }
+
+        public Object[] LoadAll()
+        {
+            return LoadAllAssets();
+        }
+
+        public Object[] LoadAllAssets()
+        {
+            return this.LoadAllAssets<Object>();
+        }
+
+        public T[] LoadAllAssets<T>() where T : Object
+        {
+            return _realBundle.LoadAllAssets<T>();
+        }
+
+        public Object[] LoadAllAssets(Type type)
+        {
+            return _realBundle.LoadAllAssets(type);
+        }
+
+        public Object[] LoadAssetWithSubAssets(string name)
+        {
+            return this.LoadAssetWithSubAssets<Object>(name);
+        }
+
+        public T[] LoadAssetWithSubAssets<T>(string name) where T : Object
+        {
+            return _realBundle.LoadAssetWithSubAssets<T>(name);
+        }
+
+        public Object[] LoadAssetWithSubAssets(string name, Type type)
+        {
+            return _realBundle.LoadAssetWithSubAssets(name, type);
+        }
+
+        public void Unload(bool unloadAllLoadedObjects)
+        {
+            _realBundle.Unload(unloadAllLoadedObjects);
+        }
+    }
+
+
+    /// <summary>
+    ///     Works just like the AssetBundleRequest type, it's just a proxy, but will use Il2CppAssetBundleRequest if it needs to so you don't have to worry about it.
+    /// </summary>
+    public class WrappedAssetBundleRequest
+    {
+        public AssetBundleRequest _realRequest;
+
+        public WrappedAssetBundleRequest(AssetBundleRequest realRequest)
+        {
+            _realRequest = realRequest;
+        }
+
+        public Object asset => _realRequest.asset;
+
+        public Object[] allAssets => _realRequest.allAssets;
+    }
+
+}
+
+/* Might need this if the above fails
+
+
+public static class Il2CppStringArrayExtensions
+{
+    public static string[] ToManagedArray(this Il2CppStringArray il2cppStrings)
+    {
+        string[] managedStrings = new string[il2cppStrings.Length];
+        for (int i = 0; i < il2cppStrings.Length; i++)
+        {
+            managedStrings[i] = il2cppStrings[i];
+        }
+        return managedStrings;
+    }
+}
+
+public static class Il2CppObjectArrayExtensions
+{
+    public static T[] ToManagedArray<T>(this Il2CppReferenceArray<T> il2cppObjects) where T : Object
+    {
+        T[] managedObjects = new T[il2cppObjects.Length];
+        for (int i = 0; i < il2cppObjects.Length; i++)
+        {
+            managedObjects[i] = il2cppObjects[i];
+        }
+        return managedObjects;
+    }
+}
+*/
+
+
